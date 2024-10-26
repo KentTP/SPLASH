be7d0c8f78f94815556c68b68ab4fb1ec9e1f641
Nathan Terranova <58537070+nterranova@users.noreply.github.com>
2024-10-26 14:02:29 -0400

    Moved rhino runner to a separate class

--- src/DesktopApp/AppRunner/AppRunner/Program.cs
+++ src/DesktopApp/AppRunner/AppRunner/Program.cs
@@ -1,21 +1,4 @@
 ﻿// Replace this path with the actual path to your Rhino executable
-using System.Diagnostics;
+using AppRunner;
 
-string rhinoPath = @"C:\Program Files\Rhino 8\System\Rhino.exe";
-
-ProcessStartInfo startInfo = new ProcessStartInfo
-{
-	FileName = rhinoPath,
-	Arguments = "", // Add any command-line arguments here if needed
-	UseShellExecute = true // Use the operating system shell to start the process
-};
-
-try
-{
-	Process rhinoProcess = Process.Start(startInfo);
-	Console.WriteLine("Rhino has been started.");
-}
-catch (Exception ex)
-{
-	Console.WriteLine($"An error occurred while starting Rhino: {ex.Message}");
-}
+RhinoRunner.LaunchRhino();
---
+++ src/DesktopApp/AppRunner/AppRunner/RhinoRunner.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppRunner
+{
+	public static class RhinoRunner
+	{
+		public static void LaunchRhino()
+		{
+			string rhinoPath = @"C:\Program Files\Rhino 8\System\Rhino.exe";
+
+			ProcessStartInfo startInfo = new ProcessStartInfo
+			{
+				FileName = rhinoPath,
+				Arguments = "", // Add any command-line arguments here if needed
+				UseShellExecute = true // Use the operating system shell to start the process
+			};
+
+			try
+			{
+				Process rhinoProcess = Process.Start(startInfo);
+				Console.WriteLine("Rhino has been started.");
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine($"An error occurred while starting Rhino: {ex.Message}");
+			}
+		}
+	}
+}
